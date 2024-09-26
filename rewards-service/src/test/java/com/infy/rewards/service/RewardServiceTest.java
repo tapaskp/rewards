d7c3032e72aa28d3ca2c0368c8ba640bc74cd10c
tapaskp <pattanaik.tapas@gmail.com>
2024-09-26 22:10:19 +0530

    negetive test cases added

--- rewards-service/src/test/java/com/infy/rewards/service/RewardServiceTest.java
+++ rewards-service/src/test/java/com/infy/rewards/service/RewardServiceTest.java
@@ -1,11 +1,10 @@
 package com.infy.rewards.service;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,8 +15,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.infy.rewards.entity.Transaction;
+import com.infy.rewards.exception.UserNotFoundException;
 import com.infy.rewards.repository.TransactionRepository;
+import com.infy.rewards.util.TestUtil;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(value = RewardService.class)
@@ -29,34 +29,29 @@ public class RewardServiceTest {
 	RewardService rewardService;
 
 	@Test
-	void getRewardsNMonthsAgo() throws Exception {
+	void getRewardsNMonthsAgoTest() throws Exception {
 
 		LocalDate now = LocalDate.now();
 		LocalDate threeMonthsAgo = now.minusMonths(3);
-		List<Transaction> transactions = new ArrayList<>();
-		Transaction transaction1 = new Transaction();
-		transaction1.setId(1L);
-		transaction1.setCustomerId(1L);
-		transaction1.setAmount(120D);
-		transaction1.setDate(LocalDate.of(2024, 7, 15));
 
-		Transaction transaction2 = new Transaction();
-		transaction2.setId(2L);
-		transaction2.setCustomerId(1L);
-		transaction2.setAmount(75D);
-		transaction2.setDate(LocalDate.of(2024, 8, 10));
-
-		Transaction transaction3 = new Transaction();
-		transaction3.setId(3L);
-		transaction3.setCustomerId(1L);
-		transaction3.setAmount(200D);
-		transaction3.setDate(LocalDate.of(2024, 9, 5));
-		transactions.add(transaction1);
-		transactions.add(transaction2);
-		transactions.add(transaction3);
 		Mockito.when(transactionRepository.findByCustomerIdAndDateBetween(1L, threeMonthsAgo, now))
-				.thenReturn(transactions);
+				.thenReturn(TestUtil.buildTransaction());
 		int totalRewards = rewardService.getRewardsNMonthsAgo(1L, 3);
 		assertEquals(365, totalRewards);
+	}
+
+	@Test
+	void getRewardsNMonthsAgoTest_User_Not_Found() {
+
+		LocalDate now = LocalDate.now();
+		LocalDate threeMonthsAgo = now.minusMonths(3);
+
+		Mockito.when(transactionRepository.findByCustomerIdAndDateBetween(1L, threeMonthsAgo, now))
+				.thenReturn(new ArrayList<>());
+
+		Throwable throwable = assertThrows(Throwable.class, () -> {
+			rewardService.getRewardsNMonthsAgo(1L, 3);
+		});
+		assertEquals(UserNotFoundException.class, throwable.getClass());
 	}
 }
---
+++ rewards-service/src/test/java/com/infy/rewards/util/TestUtil.java
@@ -0,0 +1,35 @@
+package com.infy.rewards.util;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.infy.rewards.entity.Transaction;
+
+public class TestUtil {
+	public static List<Transaction> buildTransaction() {
+		List<Transaction> transactions = new ArrayList<>();
+		Transaction transaction1 = new Transaction();
+		transaction1.setId(1L);
+		transaction1.setCustomerId(1L);
+		transaction1.setAmount(120D);
+		transaction1.setDate(LocalDate.of(2024, 7, 15));
+
+		Transaction transaction2 = new Transaction();
+		transaction2.setId(2L);
+		transaction2.setCustomerId(1L);
+		transaction2.setAmount(75D);
+		transaction2.setDate(LocalDate.of(2024, 8, 10));
+
+		Transaction transaction3 = new Transaction();
+		transaction3.setId(3L);
+		transaction3.setCustomerId(1L);
+		transaction3.setAmount(200D);
+		transaction3.setDate(LocalDate.of(2024, 9, 5));
+		transactions.add(transaction1);
+		transactions.add(transaction2);
+		transactions.add(transaction3);
+		return transactions;
+	}
+
+}
