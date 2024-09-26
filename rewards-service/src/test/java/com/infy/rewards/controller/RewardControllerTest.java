63ec378e8159785935eeebc2a87b543e9106132a
tapaskp <pattanaik.tapas@gmail.com>
2024-09-26 15:28:46 +0530

    added junit

---
+++ rewards-service/src/test/java/com/infy/rewards/controller/RewardControllerTest.java
@@ -0,0 +1,46 @@
+package com.infy.rewards.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.infy.rewards.service.RewardService;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = RewardController.class)
+@WithMockUser
+public class RewardControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private RewardService rewardService;
+
+	@Test
+	void getRewardsNMonthsAgo() throws Exception {
+
+		Mockito.when(rewardService.getRewardsNMonthsAgo(Mockito.anyLong(), Mockito.anyLong())).thenReturn(365);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/rewards/1/3/total")
+				.accept(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		System.out.println(result.getResponse().getContentAsString());
+
+		assertEquals("365", result.getResponse().getContentAsString());
+	}
+}
---
+++ rewards-service/src/test/java/com/infy/rewards/service/RewardServiceTest.java
@@ -0,0 +1,62 @@
+package com.infy.rewards.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.infy.rewards.entity.Transaction;
+import com.infy.rewards.repository.TransactionRepository;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = RewardService.class)
+@WithMockUser
+public class RewardServiceTest {
+	@MockBean
+	private TransactionRepository transactionRepository;
+	@Autowired
+	RewardService rewardService;
+
+	@Test
+	void getRewardsNMonthsAgo() throws Exception {
+
+		LocalDate now = LocalDate.now();
+		LocalDate threeMonthsAgo = now.minusMonths(3);
+		List<Transaction> transactions = new ArrayList<>();
+		Transaction transaction1 = new Transaction();
+		transaction1.setId(1L);
+		transaction1.setCustomerId(1L);
+		transaction1.setAmount(120D);
+		transaction1.setDate(LocalDate.of(2024, 7, 15));
+
+		Transaction transaction2 = new Transaction();
+		transaction2.setId(2L);
+		transaction2.setCustomerId(1L);
+		transaction2.setAmount(75D);
+		transaction2.setDate(LocalDate.of(2024, 8, 10));
+
+		Transaction transaction3 = new Transaction();
+		transaction3.setId(3L);
+		transaction3.setCustomerId(1L);
+		transaction3.setAmount(200D);
+		transaction3.setDate(LocalDate.of(2024, 9, 5));
+		transactions.add(transaction1);
+		transactions.add(transaction2);
+		transactions.add(transaction3);
+		Mockito.when(transactionRepository.findByCustomerIdAndDateBetween(1L, threeMonthsAgo, now))
+				.thenReturn(transactions);
+		int totalRewards = rewardService.getRewardsNMonthsAgo(1L, 3L);
+		assertEquals(365, totalRewards);
+	}
+}
