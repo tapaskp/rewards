aa813e7dd7b85cca5d522728203ea53ac1513d59
tapaskp <pattanaik.tapas@gmail.com>
2024-09-26 17:00:13 +0530

    ResponseEntity Added

--- rewards-service/src/main/java/com/infy/rewards/controller/RewardController.java
+++ rewards-service/src/main/java/com/infy/rewards/controller/RewardController.java
@@ -3,12 +3,15 @@ package com.infy.rewards.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.infy.rewards.service.RewardService; 
+import com.infy.rewards.service.RewardService;
 
 @RestController
 @RequestMapping("/api/rewards")
@@ -18,7 +21,9 @@ public class RewardController {
 	private RewardService rewardService;
 
 	@GetMapping("/{customerId}/{monthAgo}/total")
-	public int getRewardsNMonthsAgo(@PathVariable Long customerId, @PathVariable Integer monthAgo) {
-		return rewardService.getRewardsNMonthsAgo(customerId, monthAgo);
+	public ResponseEntity<Integer> getRewardsNMonthsAgo(@PathVariable Long customerId, @PathVariable Integer monthAgo) {
+		return new ResponseEntity<>(rewardService.getRewardsNMonthsAgo(customerId, monthAgo), HttpStatus.OK);
+
 	}
+
 }
