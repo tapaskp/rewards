73681879d4406b36b9c27bb59744c92601defebc
tapaskp <pattanaik.tapas@gmail.com>
2024-09-27 10:15:30 +0530

    Implemented lombok

--- rewards-service/src/main/java/com/infy/rewards/entity/Customer.java
+++ rewards-service/src/main/java/com/infy/rewards/entity/Customer.java
@@ -4,43 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-
-	public Customer() {
-
-	}
-
-	public Customer(Long id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + "]";
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
