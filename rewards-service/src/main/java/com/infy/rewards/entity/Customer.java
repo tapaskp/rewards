388b663ba07365ce88d440485dcf34a30cf9c0f3
tapaskp <pattanaik.tapas@gmail.com>
2024-09-27 10:23:47 +0530

    lombok added

--- rewards-service/src/main/java/com/infy/rewards/entity/Customer.java
+++ rewards-service/src/main/java/com/infy/rewards/entity/Customer.java
@@ -19,4 +19,5 @@ public class Customer {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
 }
--- rewards-service/src/main/java/com/infy/rewards/entity/Transaction.java
+++ rewards-service/src/main/java/com/infy/rewards/entity/Transaction.java
@@ -6,8 +6,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Transaction {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,53 +23,5 @@ public class Transaction {
 	private Double amount;
 	private LocalDate date;
 	private Long customerId;
-
-	public Transaction() {
-
-	}
-
-	public Transaction(Long id, Double amount, LocalDate date, Long customerId) {
-		this.id = id;
-		this.amount = amount;
-		this.date = date;
-		this.customerId = customerId;
-	}
-
-	@Override
-	public String toString() {
-		return "Transaction [id=" + id + ", amount=" + amount + ", date=" + date + ", customerId=" + customerId + "]";
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-
-	public Long getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
-	}
 
 }
